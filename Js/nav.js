53ee3be0dacdae3fdf2fd124fb15fe7c28ad93ff
brayan0113 <71518316+brayan0113@users.noreply.github.com>
2022-02-09 13:40:21 -0500

    Add files via upload
    -Actualizado proyecto #2
    -Corregido el padding de la sección hero
    -Corregido el nav responsive

--- Js/nav.js
+++ Js/nav.js
@@ -1,15 +1,21 @@
 document.getElementById("btn_i").addEventListener("click", showNav);
 document.getElementById("nav").addEventListener("click", closeNav);
+document.getElementById("margin").addEventListener("click", topMargin)
 
 nav = document.getElementById("nav");
 background = document.getElementById("back-menu");
 
 function showNav() {
-    nav.style.right = "0px";
     background.style.display = "block";
+    document.getElementById("nav").style.width = "75%";
+
 
 }
 function closeNav() {
-    nav.style.right = "-280px";
-    background.style.display = "none";
+  document.getElementById("nav").style.width = "0%";
+  background.style.display = "none";
+}
+
+function topMargin(){
+    document.getElementById("top").style.paddingTop = "75px";
 }
